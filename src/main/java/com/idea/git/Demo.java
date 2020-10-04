8af2226a5136995af1c9b89c4256bb7938418bbc
jjq_glb <jjq_glb@git.com>
2020-10-04 19:02:28 +0800

    dev3分支上添加，git test dev3

--- src/main/java/com/idea/git/Demo.java
+++ src/main/java/com/idea/git/Demo.java
@@ -5,5 +5,6 @@ public class Demo {
     public static void main(String[] args){
         System.out.println("git test bbc");
         System.out.println("git test aaa");
+        System.out.println("git test dev3");
     }
 }
